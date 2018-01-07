32b5e9cdaf1f6544873fddf57b0f42d389210331
Žilvinas Rudžionis <zilvinas.rudzionis@adform.com>
2018-01-07 18:45:02 +0200

    Separate components.

--- src/menu/menu.component.tsx
+++ src/menu/menu.component.tsx
@@ -6,24 +6,78 @@ import { Product } from "./product.model";
 import * as styles from "./menu.style.scss";
 
 
-const MenuItem = (item: Category|Product): JSX.Element => {
-  let prefix = item instanceof Category ? "Category" : "Product";
-  let hasChildren = item instanceof Category && item.children.length > 0;
-  let children = hasChildren ? (item as Category).children.map(MenuItem) : [];
-  return (
-    <li key={ prefix + item.id.toString() }>
-      { hasChildren &&
-        <span className={ styles.indicator + " glyphicon glyphicon-plus-sign" }></span>
-      }
-      { item.name }
-      { hasChildren &&
-        <ul>
-         { children }
-        </ul>
-      }
-    </li>
-  );
+export class ProductMenuItemComponent extends React.Component<{item: Product, active: boolean}, {}> {
+  constructor(props: any) {
+      super(props);
+  }
+
+  render() {
+    let item = this.props.item;
+    return (
+      <li>
+        { item.name }
+      </li>
+    );
+  }
 }
+
+
+export class CategoryMenuItemComponent extends React.Component<{item: Category, isExpanded: boolean}, {isExpanded: boolean}> {
+  constructor(props: any) {
+      super(props);
+      this.state = { isExpanded: this.props.isExpanded };
+      this.handleClick = this.handleClick.bind(this);
+      this.getChild = this.getChild.bind(this);
+  }
+
+  handleClick(event: React.MouseEvent<HTMLLIElement>) {
+    let hasChildren = this.props.item.children.length > 0;
+    if (hasChildren) {
+      this.setState({ isExpanded: !this.state.isExpanded });
+    }
+  }
+
+  getChild(child: Category|Product): React.ReactElement<CategoryMenuItemComponent|ProductMenuItemComponent> {
+    if (child instanceof Category) {
+      return React.createElement(CategoryMenuItemComponent, {
+        key: "Category " + child.id.toString(),
+        item: child,
+        isExpanded: this.state.isExpanded
+      });
+    } else {
+      return React.createElement(ProductMenuItemComponent, {
+        key: "Product " + child.id.toString(),
+        item: child
+      });
+    }
+  }
+
+  render() {
+    let item = this.props.item;
+    let hasChildren = item.children.length > 0;
+    let children = item.children.map(this.getChild);
+    let spanClass = styles.indicator + " glyphicon";
+    if (this.state.isExpanded) {
+      spanClass += " glyphicon-plus-sign";
+    } else {
+      spanClass += " glyphicon-minus-sign";
+    }
+    return (
+      <li onClick={ this.handleClick }>
+        { hasChildren &&
+          <span className={ spanClass }></span>
+        }
+        { item.name }
+        { hasChildren &&
+          <ul>
+           { children }
+          </ul>
+        }
+      </li>
+    );
+  }
+}
+
 
 export class MenuComponent extends React.Component<any, {category: Category}> {
   constructor(props: any){
@@ -46,7 +100,7 @@ export class MenuComponent extends React.Component<any, {category: Category}> {
           <div className="col-md-4">
             <ul className={ styles.tree }>
               { this.state !== null &&
-                MenuItem(this.state.category)
+                <CategoryMenuItemComponent item={this.state.category} isExpanded={true}/>
               }
             </ul>
           </div>
